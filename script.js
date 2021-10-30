95d7df8f5482099ab9296e769ede0d99f1af0f27
Roman Kolivashko <rkolivashko@gmail.com>
2021-10-30 14:28:26 -0700

    first commit, map is drawn

---
+++ script.js
@@ -0,0 +1,73 @@
+let countyURL = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json'
+let educationURL = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json'
+
+let countyData
+let educationData
+
+let canvas = d3.select('#canvas')
+let tooltip = d3.select('#tooltip')
+
+let drawMap = () => {
+   
+    canvas.selectAll('path')
+        .data(countyData)
+        .enter()
+        .append('path')
+        .attr('d', d3.geoPath())
+        .attr('class', 'county')
+        .attr('fill', (countyDataItem)=> {
+            let id = countyDataItem['id']
+            let county = educationData.find((item) =>{
+                return item['fips'] === id
+            })
+            let percentage = county['bachelorsOrHigher']
+            if (percentage <= 15){
+                return 'tomato'
+            } else if (percentage <=30){
+                return 'orange'
+            } else if (percentage <= 45) {
+                return 'lightgreen'
+            }else {
+              return 'limegreen'
+            }
+        })
+        .attr('data-fips', (countyDataItem) => {
+            return countyDataItem['id']
+        })
+        .attr('data-education', (countyDataItem) => {
+          let id = countyDataItem['id']
+          let county = educationData.find((item) =>{
+              return item['fips'] === id
+          })
+          let percentage = county['bachelorsOrHigher']
+          return percentage
+        })
+  }
+
+d3.json(countyURL).then(
+  (data, error) => {
+      if(error){
+          console.log(error)
+      }else{
+          countyData = data
+          countyData = topojson.feature(countyData, countyData.objects.counties).features
+          console.log('County Data')
+          console.log(countyData)
+
+          d3.json(educationURL).then(
+              (data, error) => {
+                  if(error){
+                      console.log(error)
+                  }
+                  else{
+                      educationData = data
+                      console.log('Education Data')
+                      console.log(educationData)
+                      drawMap()
+                  }
+              }
+          )
+
+      }
+  }
+)
